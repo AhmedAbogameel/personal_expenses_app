3dc46825fee2011d66d0923c3183a4f7980fe506
AhmedAbogameel <gamilco81@gmail.com>
2020-05-12 06:24:07 +0200

    personal_expenses_app

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,55 +9,52 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 225,
-      child: transactions.isEmpty
-          ? Column(
-              children: <Widget>[
-                Text(
-                  'No transactions added yet!',
-                  style: Theme.of(context).textTheme.title,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                    height: 150,
-                    child: Image.asset(
-                      'images/sleeping.png',
-                      fit: BoxFit.cover,
-                      color: Theme.of(context).primaryColor,
-                    )),
-              ],
-            )
-          : ListView.builder(
-              itemCount: transactions.length,
-              itemBuilder: (ctx, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(vertical: 8,horizontal: 5),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: FittedBox(
-                          child: Text('\$${transactions[index].amount}'),
-                        ),
+    return transactions.isEmpty
+        ? Column(
+            children: <Widget>[
+              Text(
+                'No transactions added yet!',
+                style: Theme.of(context).textTheme.title,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                  height: 150,
+                  child: Image.asset(
+                    'images/sleeping.png',
+                    fit: BoxFit.cover,
+                    color: Theme.of(context).primaryColor,
+                  )),
+            ],
+          )
+        : ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(vertical: 8,horizontal: 5),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: FittedBox(
+                        child: Text('\$${transactions[index].amount}'),
                       ),
                     ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transactions[index].date)
-                    ),
-                    trailing: IconButton(icon: Icon(Icons.delete),color: Theme.of(context).errorColor, onPressed: ()=> deleteTx(transactions[index].id)),
                   ),
-                );
-              },
-            ),
-    );
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date)
+                  ),
+                  trailing: IconButton(icon: Icon(Icons.delete),color: Theme.of(context).errorColor, onPressed: ()=> deleteTx(transactions[index].id)),
+                ),
+              );
+            },
+          );
   }
 }
