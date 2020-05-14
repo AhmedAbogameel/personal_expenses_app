5ae647ebde9d591c9ccec000dffe2e1fdddc557d
AhmedAbogameel <gamilco81@gmail.com>
2020-05-14 04:20:17 +0200

    personal_expenses_app

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,15 @@ import 'package:personalexpensesapp/widgets/chart.dart';
 import 'package:personalexpensesapp/widgets/new_transaction.dart';
 import 'package:personalexpensesapp/transaction.dart';
 import 'package:personalexpensesapp/widgets/transaction_list.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+//  SystemChrome.setPreferredOrientations([
+//    DeviceOrientation.portraitUp,
+//    DeviceOrientation.portraitUp
+//  ]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -13,16 +20,15 @@ class MyApp extends StatelessWidget {
       title: 'Personal Expenses',
       home: MyHomePage(),
       theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.amber,
-        textTheme: ThemeData().textTheme.copyWith(
-          title: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
-          button: TextStyle(color: Colors.white),
-        )
-      ),
+          primarySwatch: Colors.purple,
+          accentColor: Colors.amber,
+          textTheme: ThemeData().textTheme.copyWith(
+                title: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                button: TextStyle(color: Colors.white),
+              )),
     );
   }
 }
@@ -33,8 +39,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  final List<Transaction>_userTransactions = [
+  final List<Transaction> _userTransactions = [
 //    Transaction(
 //      title: 'New Shoes',
 //      amount: 45.99,
@@ -48,14 +53,18 @@ class _MyHomePageState extends State<MyHomePage> {
 //      id: 't2',
 //    ),
   ];
+  bool _showChart = false;
 
   List<Transaction> get _recentTransactions {
-    return _userTransactions.where((tx){
-      return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7)),);
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(Duration(days: 7)),
+      );
     }).toList();
   }
 
-  void _addNewTransaction(String txTitle,double txAmount,DateTime chosenDate){
+  void _addNewTransaction(
+      String txTitle, double txAmount, DateTime chosenDate) {
     final newTx = Transaction(
       title: txTitle,
       amount: txAmount,
@@ -67,21 +76,21 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _startAddNewTransaction(BuildContext ctx){
+  void _startAddNewTransaction(BuildContext ctx) {
     showModalBottomSheet(
         context: ctx,
-        builder: (_){
+        builder: (_) {
           return GestureDetector(
-            onTap: (){},
+            onTap: () {},
             behavior: HitTestBehavior.opaque,
             child: NewTransaction(_addNewTransaction),
           );
         });
   }
 
-  void _deleteTransaction(String id){
+  void _deleteTransaction(String id) {
     setState(() {
-      _userTransactions.removeWhere((tx)=> tx.id == id);
+      _userTransactions.removeWhere((tx) => tx.id == id);
     });
   }
 
@@ -90,7 +99,9 @@ class _MyHomePageState extends State<MyHomePage> {
     final appBar = AppBar(
       title: Text('Personal Expenses'),
       actions: <Widget>[
-        IconButton(icon: Icon(Icons.add), onPressed: ()=> _startAddNewTransaction(context)),
+        IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context)),
       ],
     );
     return Scaffold(
@@ -98,14 +109,38 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            Container(height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.4,child: Chart(_recentTransactions)),
-            Container(height: (MediaQuery.of(context).size.height - appBar.preferredSize.height) * 0.7,child: TransactionList(_userTransactions,_deleteTransaction)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Show Chart'),
+                Switch(
+                  value: _showChart,
+                  onChanged: (val){
+                    setState(() {
+                      _showChart = val;
+                    });
+                  },
+                ),
+              ],
+            ),
+            _showChart ?
+            Container(
+                height: (MediaQuery.of(context).size.height -
+                        appBar.preferredSize.height -
+                        MediaQuery.of(context).padding.top) *
+                    0.7,
+                child: Chart(_recentTransactions),)
+            : Container(
+                height: (MediaQuery.of(context).size.height -
+                        appBar.preferredSize.height) *
+                    0.7,
+                child: TransactionList(_userTransactions, _deleteTransaction)),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: ()=>_startAddNewTransaction(context),
+        onPressed: () => _startAddNewTransaction(context),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,37 +3,41 @@ import 'package:personalexpensesapp/transaction.dart';
 import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
-  TransactionList(this.transactions,this.deleteTx);
+  TransactionList(this.transactions, this.deleteTx);
   final List<Transaction> transactions;
   final Function deleteTx;
 
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? Column(
-            children: <Widget>[
-              Text(
-                'No transactions added yet!',
-                style: Theme.of(context).textTheme.title,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                  height: 150,
-                  child: Image.asset(
-                    'images/sleeping.png',
-                    fit: BoxFit.cover,
-                    color: Theme.of(context).primaryColor,
-                  )),
-            ],
+        ? LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(
+                children: <Widget>[
+                  Text(
+                    'No transactions added yet!',
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                      height: constraints.maxHeight * 0.6,
+                      child: Image.asset(
+                        'images/sleeping.png',
+                        fit: BoxFit.cover,
+                        color: Theme.of(context).primaryColor,
+                      )),
+                ],
+              );
+            },
           )
         : ListView.builder(
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               return Card(
                 elevation: 5,
-                margin: EdgeInsets.symmetric(vertical: 8,horizontal: 5),
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
                 child: ListTile(
                   leading: CircleAvatar(
                     radius: 30,
@@ -48,10 +52,12 @@ class TransactionList extends StatelessWidget {
                     transactions[index].title,
                     style: Theme.of(context).textTheme.title,
                   ),
-                  subtitle: Text(
-                    DateFormat.yMMMd().format(transactions[index].date)
-                  ),
-                  trailing: IconButton(icon: Icon(Icons.delete),color: Theme.of(context).errorColor, onPressed: ()=> deleteTx(transactions[index].id)),
+                  subtitle:
+                      Text(DateFormat.yMMMd().format(transactions[index].date)),
+                  trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Theme.of(context).errorColor,
+                      onPressed: () => deleteTx(transactions[index].id)),
                 ),
               );
             },
